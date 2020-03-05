e764c145b9d567aaa3454e70d8b733dc96ec7b39
sanjay <sanjayzzzhong@gmail.com>
2020-03-05 11:10:47 +0800

    update from macos

--- intro/daytimetcpcli.c
+++ intro/daytimetcpcli.c
@@ -9,19 +9,22 @@ main(int argc, char **argv)
 
 	if (argc != 2)
 		err_quit("usage: a.out <IPaddress>");
-
+	// AF_INET是网际协议， SOCK_STREAM是字节流，tcp就是字节流的
 	if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 		err_sys("socket error");
-
+	// 先置0
 	bzero(&servaddr, sizeof(servaddr));
+	// 协议家族
 	servaddr.sin_family = AF_INET;
+	// 绑定端口号
 	servaddr.sin_port   = htons(13);	/* daytime server */
+	// 绑定ip地址
 	if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
 		err_quit("inet_pton error for %s", argv[1]);
-
+	// 连接
 	if (connect(sockfd, (SA *) &servaddr, sizeof(servaddr)) < 0)
 		err_sys("connect error");
-
+	// 读取
 	while ( (n = read(sockfd, recvline, MAXLINE)) > 0) {
 		recvline[n] = 0;	/* null terminate */
 		if (fputs(recvline, stdout) == EOF)
